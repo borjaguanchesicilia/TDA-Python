bbe3b4ebc3bd0534a7345f920fa9510e943e5ab1
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-10 23:23:08 +0000

    TEST 3

--- ListaSimplementeEnlazada/ListaEnlazada.py
+++ ListaSimplementeEnlazada/ListaEnlazada.py
@@ -1,18 +1,26 @@
 class ListaEnlazada:
 
     def __init__(self):
-
         self.cabeza = None
 
     def insertarCabeza(self, nodo):
         nodo.setSiguiente(self.cabeza)
         self.cabeza = nodo
 
+    def insertarN(self, anteriorNodo, nuevoNodo):
+        nuevoNodo.setSiguiente(anteriorNodo.getSiguiente())
+        anteriorNodo.setSiguiente(nuevoNodo)
+
     def queHayEnCabeza(self):
         return self.cabeza.getDato
 
     def recorrerLista(self):
         aux = self.cabeza
+        print ("\nLa lista es: \n")
         while(aux != None):
-            print ("El dato  es: " + str(aux.getDato()) + "\n\n")
+            if aux.getSiguiente() != None:
+                print (str(aux.getDato()) + " --> ",end="")
+            else:
+                print (str(aux.getDato()),end="")
             aux = aux.getSiguiente()
+        print ("\n\n")
