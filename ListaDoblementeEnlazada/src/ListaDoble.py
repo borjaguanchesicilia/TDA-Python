0f2677b1c91c952ba75f2482598b039f5102fe97
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-11 16:17:44 +0000

    TEST 4

--- ListaDoblementeEnlazada/src/ListaDoble.py
+++ ListaDoblementeEnlazada/src/ListaDoble.py
@@ -1,5 +1,5 @@
 class ListaDoblementeEnlazada:
-    
+
     def __init__(self):
         self.cabeza = None
         self.cola = None
@@ -10,14 +10,16 @@ class ListaDoblementeEnlazada:
             self.cabeza = self.cola = nodo
         else:
             nodo.siguiente = self.cabeza
+            self.cola.anterior = nodo
             self.cabeza = nodo
         self.tam += 1
 
     def insertarCola(self, nodo):
         if self.tam == 0:
-            self.cola = nodo
+            self.cabeza = self.cola = nodo
         else:
             nodo.anterior = self.cola
+            self.cola.siguiente = nodo
             self.cola = nodo
         self.tam += 1
 
@@ -26,3 +28,25 @@ class ListaDoblementeEnlazada:
 
     def queHayEnCola(self):
         print ("\nEn la cola hay: " + str(self.cola.dato))
+
+    def extraerCabeza(self):
+        aux = self.cabeza
+        self.cabeza = self.cabeza.siguiente
+        if self.cabeza != None:
+            self.cabeza.anterior = None
+        else:
+            self.cola = None
+
+        self.tam -=1
+        aux.siguiente = None
+
+    def recorrerLista(self):
+        aux = self.cabeza
+        print ("\nLa lista es: \n")
+        while(aux != None):
+            if (aux.siguiente != None):
+                print (str(aux.dato) + " <--> ",end="")
+            else:   
+                print (str(aux.dato),end="")
+            aux = aux.siguiente
+        print ("\n")
