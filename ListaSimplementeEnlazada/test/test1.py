d737f4a5291de5f37fb2d43cc9d331bff24bed14
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-12 17:54:51 +0000

    TEST 1

--- ListaSimplementeEnlazada/test/test1.py
+++ ListaSimplementeEnlazada/test/test1.py
@@ -2,10 +2,10 @@ import Nodo as n
 
 n1 = n.Nodo(15)
 
-print ("El dato del nodo 1 es: " + str(n1.getDato()))
-print ("El siguiente nodo del nodo 1 es: " + str(n1.getSiguiente()))
+print ("El dato del nodo 1 es: " + str(n1.dato))
+print ("El siguiente nodo del nodo 1 es: " + str(n1.siguiente))
 
 n2 = n.Nodo(16)
-n1.setSiguiente(n2)
-print ("El dato del nodo 2 es: " + str(n2.getDato()))
-print (n1.getSiguiente())
+n1.siguiente = n2
+print ("El dato del nodo 2 es: " + str(n2.dato))
+print (n1.siguiente)
