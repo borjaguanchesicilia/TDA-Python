313471b543a097ec662ca56b63acd679cd8aefbc
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-12 13:06:06 +0000

    TEST 2

--- ListaDoblementeEnlazada/test/test2.py
+++ ListaDoblementeEnlazada/test/test2.py
@@ -1,6 +1,8 @@
 import NodoD as n
 import ListaDoble as l
 
+print ("\n\nProbamos el método insertar por cabeza, y los qué hay en cabezay en cola.")
+
 ListaDoble = l.ListaDoblementeEnlazada()
 
 n1 = n.NodoD(1)
