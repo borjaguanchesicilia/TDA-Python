45564688d2856592c9ac7b3b42dbbdc7fe4dbf51
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-12 19:05:15 +0000

    TEST 2

--- Cola/src/Cola.py
+++ Cola/src/Cola.py
@@ -1,22 +1,27 @@
 class Cola:
-
+    
     def __init__(self):
         self.vect = []
-        self.frente = 0
-        self.final = -1
+        self.primero = 0
+        self.ultimo = -1
 
     def vacia(self):
-        if self.final < self.frente:
+        if self.ultimo < self.primero:
             print ("\nLa cola está vacía.")
         else:
             print ("\nLa cola NO está vacía.")
 
     def tamCola(self):
-        print ("\nEl tamaño de la cola es: " + str((self.final - self.frente) + 1))
+        print ("\nEl tamaño de la cola es: " + str((self.ultimo - self.primero) + 1))
 
     def push(self, dato):    
-        self.final  += 1
+        self.ultimo  += 1
         self.vect.append(dato)
+
+    def pop(self):    
+        self.primero  += 1
+        self.vect.pop(0)
+        self.vect.insert(0, ' ')
     
     def mostrar(self):
         print ("\nLa cola es:\n")
---
+++ Cola/test/test2.py
@@ -0,0 +1,18 @@
+import Cola as c
+
+miCola = c.Cola()
+
+miCola.vacia()
+
+miCola.push(1)
+miCola.push(2)
+miCola.push(3)
+miCola.push(4)
+
+miCola.vacia()
+
+miCola.mostrar()
+
+miCola.pop()
+
+miCola.mostrar()
