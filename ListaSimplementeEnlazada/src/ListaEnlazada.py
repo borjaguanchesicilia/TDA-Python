c7597cc0ef022d5dd379ba80c06c038626de92de
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-12 17:46:14 +0000

    ACT assert

--- ListaSimplementeEnlazada/src/ListaEnlazada.py
+++ ListaSimplementeEnlazada/src/ListaEnlazada.py
@@ -1,55 +1,49 @@
-import sys
-
 class ListaEnlazada:
-
+    
     def __init__(self):
         self.cabeza = None
 
     def insertarCabeza(self, nodo):
-        nodo.setSiguiente(self.cabeza)
+        nodo.siguiente = self.cabeza
         self.cabeza = nodo
 
     def insertarN(self, anteriorNodo, nuevoNodo):
-        nuevoNodo.setSiguiente(anteriorNodo.getSiguiente())
-        anteriorNodo.setSiguiente(nuevoNodo)
+        nuevoNodo.siguiente = anteriorNodo.siguiente
+        anteriorNodo.siguiente = nuevoNodo
 
     def queHayEnCabeza(self):
-        print ("\nEn la cabeza hay: " + str(self.cabeza.getDato()))
+        print ("\nEn la cabeza hay: " + str(self.cabeza.dato))
 
     def extraerCabeza(self):
+        assert self.cabeza != None, 'No hay ningún nodo en la lista.'
         aux = self.cabeza
-        self.cabeza = self.cabeza.getSiguiente()
-        aux.setSiguiente(None)
+        self.cabeza = self.cabeza.siguiente
+        aux.siguiente = None
 
     def extraerN(self, anteriorNodo):
-        if anteriorNodo == None:
-            sys.exit()
-        else:
-            aux = anteriorNodo.getSiguiente()
-            if anteriorNodo == None:
-                sys.exit()
-            else:
-                anteriorNodo.setSiguiente(aux.getSiguiente())
+        assert anteriorNodo != None, 'No hay ningún nodo en la lista.'
+        aux = anteriorNodo.siguiente
+        anteriorNodo.siguiente = aux.siguiente
 
     def recorrerLista(self):
         aux = self.cabeza
         print ("\nLa lista es: \n")
         while(aux != None):
-            if aux.getSiguiente() != None:
-                print (str(aux.getDato()) + " --> ",end="")
+            if aux.siguiente != None:
+                print (str(aux.dato) + " --> ",end="")
             else:
-                print (str(aux.getDato()),end="")
-            aux = aux.getSiguiente()
+                print (str(aux.dato),end="")
+            aux = aux.siguiente
         print ("\n")
 
     def buscarNodo(self, valor):
         aux = self.cabeza
         valorEncontrado = 0
         while ((aux != None) and (valorEncontrado == 0)):
-            if aux.getDato() == valor:
+            if aux.dato == valor:
                 valorEncontrado = 1
             else:
-                aux = aux.getSiguiente()
+                aux = aux.siguiente
 
         if valorEncontrado == 1:
             print("\nEl valor fue encontrado.")
