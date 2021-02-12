1e8ca680b789420b34e8624c2b97b67420ded964
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-02-12 18:01:39 +0000

    ACT assert

--- Pila/src/Pila.py
+++ Pila/src/Pila.py
@@ -9,16 +9,19 @@ class Pila:
             print ("La pila está vacía.")
         else:
             print ("La pila NO está vacía.")
+        return self.top
 
     def push(self, dato):
         self.top += 1
         self.vect.append(dato)
 
     def pop(self):
+        assert self.top > 0, 'La pila está vacía.'
         self.top -= 1
         self.vect.pop()
 
     def queHayEnTop(self):
+        assert self.top > 0, 'La pila está vacía.'
         print  ("\nEn el  top hay: " + str(self.vect.index(self.top)))
 
     def mostrar(self):
